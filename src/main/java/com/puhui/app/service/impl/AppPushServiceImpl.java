f01083723e494acba4732b89629d299d31318082
lichunyue <lichunyue@puhuifinance.com>
2016-12-05 15:47:32 +0800

    删除无用代码

--- src/main/java/com/puhui/app/service/impl/AppPushServiceImpl.java
+++ src/main/java/com/puhui/app/service/impl/AppPushServiceImpl.java
@@ -133,8 +133,6 @@ public class AppPushServiceImpl implements AppPushService {
 			pushModel = appLendTemplate.getTempletContent();
 			AppCustomer appCustomer = appCustomerDao.query(Long.parseLong(map.get("uid").toString()));// 获取AppCustomer对象
 			RemoteLendAppResultVo remoteLendAppResultVo = remoteLendAppUserCenterService.getUserInfoMethod(Long.parseLong(map.get("sellerNumber").toString()));
-//			RemoteStaffVo remoteStaffVo = null;
-//			RemoteStaffVo remoteStaffVo = swaggerService.ucId(Long.parseLong(map.get("sellerNumber").toString()));
 			String customerName = appCustomer.getCustomerName();// 用户姓名
 			String shopName = remoteLendAppResultVo.getShopName();// 门店名字
 			String salesName = remoteLendAppResultVo.getName();// 销售姓名
