05562289f094580ed40abe8e1d7a06f7b9569b97
lichunyue <lichunyue@puhuifinance.com>
2017-01-05 17:01:27 +0800

    加密

---
+++ src/main/java/com/puhui/app/utils/LendAesUtil.java
@@ -0,0 +1,51 @@
+package com.puhui.app.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.puhui.aes.AesEncryptionUtil;
+
+/**
+ * 加密解密工具类
+ * @author  yangzhiqiang
+ *
+ */
+public class LendAesUtil {
+    private static final Logger log = LoggerFactory.getLogger(LendAesUtil.class);
+
+	/**
+	 * 加密
+	 * 加密方法返回值会有“xy”前缀
+	 * @param cardNo
+	 * @return
+	 */
+	public static String encrypt(String content){
+		if(StringUtils.isEmpty(content)){
+			log.info("加密字符串为null");
+			return content;
+		}
+		if(content.startsWith("xy") && content.length() > 18){
+			return content;
+		}
+		return AesEncryptionUtil.encrypt(content);
+	}
+
+	/**
+	 * 解密
+	 * 解密方法需要传入带有“xy”前缀的字符串，才能解密；如果没有前缀则不会处理，直接返回原始字符串
+	 * @param cardNo
+	 * @return
+	 */
+	public static String decrypt(String content){
+		if(StringUtils.isEmpty(content)){
+			log.info("解密字符串为null");
+			return content;
+		}
+		if(content.startsWith("xy") && content.length() < 18){
+			return content;
+		}
+		return AesEncryptionUtil.decrypt(content);
+	}
+	
+}
