cd8c6c065e3c302112fa6159c3b4bfdbfae8b450
lichunyue <lichunyue@puhuifinance.com>
2017-03-21 12:30:43 +0800

    修改状态

--- src/main/java/com/puhui/app/dao/AppLendRequestDao.java
+++ src/main/java/com/puhui/app/dao/AppLendRequestDao.java
@@ -9,14 +9,13 @@ import com.puhui.app.po.AppLendRequest;
 
 public interface AppLendRequestDao extends BaseDao{
 
-	public void updateCustomerEntryState(long id);
+	public void updateCustomerEntryState(Map<String, Object> map);
 	
 	public List<String> selectAppLendRequestMethod(Map<String,Object> map);
 	
 	public void updateCustomerSalesNo(Map<String,Object> map);
 	/**
      * 获取AppLendRequest
-     * @param CustomerId
      * @return
      */
 	public AppLendRequest getAppLendRequestByCustomerId(Long appCustomerId);
