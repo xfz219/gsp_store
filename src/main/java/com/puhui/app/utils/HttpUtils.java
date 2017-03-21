a119ade6a67825ca8976c2f78ada137b12724e7a
lichunyue <lichunyue@puhuifinance.com>
2017-03-21 12:22:28 +0800

    增加空格

--- src/main/java/com/puhui/app/utils/HttpUtils.java
+++ src/main/java/com/puhui/app/utils/HttpUtils.java
@@ -8,9 +8,9 @@ public class HttpUtils {
     private static final String PUHUI_LEND_DEDUCTION_URL = "https://api.puhuifinance.com/puhui-lend-server";
 //    private static final String PUHUI_LEND_DEDUCTION_URL = "http://ut1.zuul.pub.puhuifinance.com:8765/puhui-lend-server";
     
-    public static final String PUHUI_LEND_URL_ID = PUHUI_LEND_DEDUCTION_URL + "/api/v1/{appLendRequestId}/appLendRequest"; //通过app进件号获取进件信息
+    public static final String PUHUI_LEND_URL_ID = PUHUI_LEND_DEDUCTION_URL + "/api/v1/{appLendRequestId}/appLendRequest";//通过app进件号获取进件信息
     public static final String PUHUI_LEND_URL_LOGIN = PUHUI_LEND_DEDUCTION_URL + "/api/uc/v1/staff/login"; //登陆认证
-    public static final String PUHUI_LEND_URL_EMPLOYEENO = PUHUI_LEND_DEDUCTION_URL + "/api/uc/v1/staff/{employeeNo}/employeeNo"; //根据员工编号获取UC中用户信息
+    public static final String PUHUI_LEND_URL_EMPLOYEENO = PUHUI_LEND_DEDUCTION_URL + "/api/uc/v1/staff/{employeeNo}/employeeNo";//根据员工编号获取UC中用户信息
     public static final String PUHUI_LEND_URL_TOP = PUHUI_LEND_DEDUCTION_URL + "/api/uc/v1/menu/top"; //查询顶级菜单
     public static final String PUHUI_LEND_URL_SUB = PUHUI_LEND_DEDUCTION_URL + "/api/uc/v1/menu/{menusId}/sub"; //查询下级菜单
     public static final String PUHUI_LEND_URL_UCID = PUHUI_LEND_DEDUCTION_URL + "/api/uc/v1/staff/{id}/id"; //根据ID获取UC中用户信息
