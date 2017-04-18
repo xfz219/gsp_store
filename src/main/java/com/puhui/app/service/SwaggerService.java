a8ec77b7e2a1c30c2b3b065e8376a3d0ad1b9140
yanghuili <yanghuili@puhuifinance.com>
2017-04-18 11:29:32 +0800

    modify mq config of uat env

--- src/main/java/com/puhui/app/service/SwaggerService.java
+++ src/main/java/com/puhui/app/service/SwaggerService.java
@@ -1,22 +1,19 @@
 package com.puhui.app.service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
+import com.puhui.app.utils.HttpUtils;
+import com.puhui.lend.vo.ResponseVo;
+import com.puhui.lend.vo.userCenter.UCRbacPermissionVo;
+import com.puhui.uc.vo.DataGrid;
+import com.puhui.uc.vo.RemoteOrganizationVo;
+import com.puhui.uc.vo.RemoteStaffVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 
-import com.puhui.app.utils.HttpUtils;
-import com.puhui.lend.vo.LendRequestVo;
-import com.puhui.lend.vo.ResponseVo;
-import com.puhui.lend.vo.userCenter.UCRbacPermissionVo;
-import com.puhui.uc.vo.DataGrid;
-import com.puhui.uc.vo.RemoteOrganizationVo;
-import com.puhui.uc.vo.RemoteStaffVo;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author lcy
@@ -28,23 +25,7 @@ public class SwaggerService {
 
 	@Autowired
 	private OauthService oauthService;
-	/**
-	 * 获客个贷系统数据
-	 * @param appLendRequestId
-	 * @return
-	 */
-	public Map<String,Object> getLendRequestIdMethod(Long appLendRequestId) {
-		String url = HttpUtils.PUHUI_LEND_URL_ID;
-		ResponseVo<LendRequestVo> responseIdVo = oauthService.jsonGet(url, ResponseVo.class,
-				new Object[] { appLendRequestId });
-		if (responseIdVo.getCode().equals(SUCCEED)) {
-			Map<String,Object> linkedHashMap = (Map) responseIdVo.getResult();
-			return linkedHashMap;
-		} else {
-			return null;
-		}
-	}
-	
+
 	/**
 	 * 登陆认证
 	 * @param salesNo
