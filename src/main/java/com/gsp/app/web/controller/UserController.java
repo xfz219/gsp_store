34b3345a492f6eeedfb8119124c9711f4daa1ee4
fengzhix.xu <fengzhix.xu@qunar.com>
2018-05-19 23:03:06 +0800

    Merge branch 'master' of https://github.com/xfz219/gsp_store
    # Conflicts:
    #	pom.xml

---
+++ src/main/java/com/gsp/app/web/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.gsp.app.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * fengzhix.xu on 2018/5/19.
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+}
