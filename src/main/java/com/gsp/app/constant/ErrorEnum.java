38e9d75744f017815c9f50d74dbf2b97e1ac5cf9
fengzhix.xu <fengzhix.xu@qunar.com>
2018-05-19 22:55:22 +0800

    Merge branch 'master' of https://github.com/xfz219/gsp_store
    # Conflicts:
    #	pom.xml

--- src/main/java/com/gsp/app/constant/ErrorEnum.java
+++ src/main/java/com/gsp/app/constant/ErrorEnum.java
@@ -1,8 +1,13 @@
 package com.gsp.app.constant;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * fengzhix.xu on 2018/5/19.
  */
+@Getter
+@AllArgsConstructor
 public enum ErrorEnum {
 
     PWD_ERROR(301, "用户名密码错误"),
@@ -11,16 +16,4 @@ public enum ErrorEnum {
     private int code;
     private String message;
 
-    ErrorEnum(int code, String message) {
-        this.code = code;
-        this.message = message;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
---
+++ src/main/java/com/gsp/app/model/Role.java
@@ -0,0 +1,13 @@
+package com.gsp.app.model;
+
+import lombok.Getter;
+
+/**
+ * fengzhix.xu on 2018/5/19.
+ */
+@Getter
+public class Role {
+    private Integer id ;
+    private String des;
+
+}
